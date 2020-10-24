7c3d020cb2f2a7b17bc122a942636a41aa3229c5
Lasse Nordahl <lasseanordahl@gmail.com>
2020-10-23 17:58:22 -0700

    Updates

---
+++ precache-manifest.4812dc3c33e0e210a3c5a531d04118d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6835d2d8e2a9a369b175aa96c2d1b3ad",
+    "url": "/zothacks-frontend/index.html"
+  },
+  {
+    "revision": "fe88f320bae737519e70",
+    "url": "/zothacks-frontend/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "075a33aac85c92a5d0ed",
+    "url": "/zothacks-frontend/static/css/main.9eb656e2.chunk.css"
+  },
+  {
+    "revision": "fe88f320bae737519e70",
+    "url": "/zothacks-frontend/static/js/2.4fcf5742.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/zothacks-frontend/static/js/2.4fcf5742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075a33aac85c92a5d0ed",
+    "url": "/zothacks-frontend/static/js/main.484e99ff.chunk.js"
+  },
+  {
+    "revision": "498c7e9c36a57ec7e3de",
+    "url": "/zothacks-frontend/static/js/runtime-main.db8b604a.js"
+  }
+]);
